b25a7fbd89cbdc5111d4cd00665fb908fec73c58
IanChiu28 <nk9710632@gmail.com>
2019-11-08 12:43:56 +0800

    add jquery module

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const $ = require('jquery');
+
 /*const csvParser = require('csv-parser');
 const csvWriter = require('csv-writer').createObjectCsvWriter;
 const fs = require('fs');
